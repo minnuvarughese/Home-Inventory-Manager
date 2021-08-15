24bbc741681c32e01687162a2e1667b28d140e8b
minnuvarughese <49233625+minnuvarughese@users.noreply.github.com>
2021-08-15 23:09:19 +0530

    First Commit

---
+++ JDateChooser.java
@@ -0,0 +1,28 @@
+package homeinventory;
+
+import java.awt.Dimension;
+import java.util.Date;
+
+public class JDateChooser {
+
+	public Date getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private void myMethod () {
+		 JDateChooser j = new JDateChooser();
+		}
+	public void setPreferredSize(Dimension dimension) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void requestFocus() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setDate(Date stringToDate) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
